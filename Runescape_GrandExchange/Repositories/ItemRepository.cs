262113992b5341cd233a39fec6a5b81c03e2ecf2
Bhabiji <caerelsjas@gmail.com>
2021-05-01 17:25:03 +0200

    Added repository functions (not functional), added Category json files

---
+++ Runescape_GrandExchange/Repositories/ItemRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Runescape_GrandExchange.Repositories
+{
+    class ItemRepository
+    {
+        public static List<string> GetCategories()
+        {
+            return null;
+        }
+        public static List<string> GetItems()
+        {
+            return null;
+        }
+        public static List<string> GetItemsByFirstLetter(string letter)
+        {
+            return null;
+        }
+    }
+}
